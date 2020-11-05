e8517787abfa6c8d7dc7ac1bbc3dd50b034c2f0e
Rakesh Kumar <rkrakesh133@gmail.com>
2020-11-05 20:53:27 +0530

    Done

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rakesh Kumar, Web Developer + Designer</title>
+    <link href="https://fonts.googleapis.com/css?family=Caveat|Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <main>
+      <!-- ***********************  ABOUT / PROFILE  *********************** -->
+      <header>
+        <div class="content-wrap">
+          <h1>Rakesh Kumar</h1>
+          <h2>Web Developer + Designer</h2>
+
+          <p>As a developer, I specialize in creating modular and scalable front-end architectures.</p>
+          <!-- <p>I’m also exploring more creative pursuits designing tee shirts and accessories for <a href="http://teethang.com" target="_blank">Nuthin’ But a Tee Thang</a>, an online store I’m running with my husband.</p> -->
+        </div>
+      </header>
+
+      <!-- ********************  PROJECTS / PORTFOLIO  ********************* -->
+      <section class="projects">
+        <div class="content-wrap divider">
+          <h2>Featured Projects</h2>
+          <p>View selected projects below.</p>
+
+          <!-- Project 1 -->
+          <section class="project-item">
+            <img src="images\globe_bank_home_page.png" alt="Globe Bank Home Page"/>
+            <h3>Globe Bank CMS Part 1</h3>
+            <p>Developed a public page for fictitious bank named "GLOBE BANK".</p>
+            <a class="btn" href="http://localhost/globe_bank/public/" target="_blank">More...</a>
+          </section>
+
+          <!-- Project 2 -->
+          <section class="project-item">
+            <img src="images\globe_bank_admin_home_page.png" alt="Globe Bank Admin Page">
+            <h3>Globe Bank CMS Part 2 (Admin)</h3>
+            <p>Create, Edit, View & Delete Subjects, Pages as well as Users.</p>
+            <a class="btn" href="http://localhost/globe_bank/public/staff" target="_blank">More...</a>
+          </section>
+
+          <!-- Project 3 -->
+          <section class="project-item">
+            <img src="images\cahin_gang_inventory.png" alt="Chain Gang">
+            <h3>Chain Gang</h3>
+            <p>Chain Gang was started in 2005 by a group of bicycle loving friends. We wanted to create a neighborhood bike shop that could also deliver top-quality bicycles to your doorstep.</p>
+            <a class="btn" href="http://localhost/chain_gang/public/" target="_blank">More...</a>
+          </section>
+        </div>
+      </section>
+
+      <!-- ***********************  WORK EXPERIENCE  *********************** -->
+      <section class="work-experience">
+        <div class="content-wrap item-details divider">
+          <h2>Work Experience</h2>
+          <p>See my complete work history.</p>
+
+          <!-- Job 1 -->
+          <section class="job-item">
+            <div class="job-details">
+              <h3>Educator</h3>
+              <p>Computer Teacher - D.A.V. Public School, Anisabad, Patna - Bihar</p>
+              <p>May 2017-June 2018</p>
+            </div>
+            <div class="job-summary">
+              <p>Taught students of std. 5-7.</p>
+              <p>Key contributions:</p>
+              <ul>
+                <li>Responsible for conducting pratical session according to curriculum.</li>
+                <li>Maintaining software requirements in computer lab.</li>
+                <!-- <li>Standardized the site architecture and design.</li> -->
+              </ul>
+            </div>
+          </section>
+
+          <!-- Job 2 -->
+          <!-- <section class="job-item">
+            <div class="job-details">
+              <h3>Director of Curriculum</h3>
+              <p>Ladies Learning Code</p>
+              <p>July 2014 - February 2016</p>
+            </div>
+            <div class="job-summary">
+              <p>Managed all curriculum for the adult programs. Created teaching materials and implemented instructor training across 20+ Canadian chapters.</p>
+              <p>Key contributions:</p>
+              <ul>
+                <li>Overhauled the curriculum and standardized the content and delivery.
+                  <ul>
+                    <li>Created 9 new workshops focusing on HTML, CSS, JavaScript, jQuery, Wordpress and Responsive Web Design.</li>
+                    <li>Created all Hackapalooza content (2 day conference style event) consisting of 2-4 hour workshop sessions.</li>
+                    <li>Created interactive slide deck template for consistent national workshop branding.</li>
+                    <li>Developed the Digital Skills part-time program curriculum.</li>
+                  </ul>
+                </li>
+                <li>Provided training for all instructors focusing on creating an inclusive and engaging learning environment.</li>
+                <li>Maintained the ladieslearningcode.com website.</li>
+                <li>Volunteer lead instructor from 2011-2014 for Toronto workshops and several chapter launches.</li>
+              </ul>
+            </div>
+          </section> -->
+
+          <!-- Job 3 -->
+          <section class="job-item">
+            <div class="job-details">
+              <h3>Educator</h3>
+              <p>Computer Teacher - D.A.V. Public School, BSEB Punaichak, Patna - Bihar</p>
+              <p>June 2016 - February 2017</p>
+            </div>
+            <div class="job-summary">
+              <p>Taught students of std. 8, 9, 10.</p>
+              <p>Key contributions:</p>
+              <ul>
+                <li>Responsible for conducting pratical session according to curriculum.</li>
+                <li>Maintaining software requirements in computer lab.</li>
+                <!-- <li>Standardized the site architecture and design.</li> -->
+              </ul>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <!-- ******************  EDUCATION & CERTIFICATIONS ****************** -->
+      <section class="education item-details">
+        <!-- education -->
+        <div class="content-wrap divider">
+          <h2>Education</h2>
+
+          <section>
+            <h3>Pondicherry University - Pondicherry</h3>
+            <p>Master In Computer Application, 2018-Appearing</p>
+            <p>(MCA)</p>
+          </section>
+
+          <section>
+            <h3>IGNOU - Patna, Bihar</h3>
+            <p>Bachelor In Computer Application, 2013-2016</p>
+            <p>(BCA)</p>
+          </section>
+
+          <section>
+            <h3>SRLSYSS, High School, Punaichak, Bihar</h3>
+            <p>Senior Secondry High School(12<sup>th</sup>), 2012</p>
+          </section>
+        </div>
+
+        <!-- Certifications -->
+        <div class="content-wrap divider">
+            <h2>Certifications</h2>
+
+            <!-- certificate 1 -->
+            <section class="certificate-item">
+              <div class="certificate-details">
+                <h3>PHP Essential Training</h3>
+                <p>Completed this course on January 10, 2020</p>
+              </div>
+              <div class="certificate-summary">
+                <p>Learned Topic:</p>
+                <ul>
+                  <li>What is PHP?</li>
+                  <li>Embedding PHP code on a page</li>
+                  <li>Inserting code comments</li>
+                  <li>Variables, strings, arrays, and Booleans</li>
+                  <li>If, else, and elseif statements</li>
+                  <li>While and for loops</li>
+                  <li>User-defined functions</li>
+                  <li>Function arguments and return values</li>
+                  <li>Debugging and troubleshooting</li>
+                </ul>
+              </div>
+            </section>
+
+            <!-- certificate 2 -->
+            <section class="certificate-item">
+              <div class="certificate-details">
+                <h3>Learning PHP</h3>
+                <p>Completed this course on December 10, 2019</p>
+              </div>
+              <div class="certificate-summary">
+                <p>Learned Topic:</p>
+                <ul>
+                  <li>Naming variables</li>
+                  <li>Storing text as strings</li>
+                  <li>Doing calculations with PHP</li>
+                  <li>Using conditional statements to make decisions</li>
+                  <li>Creating custom functions</li>
+                  <li>Deciphering error messages</li>
+                  <li>Emailing the contents of an online form</li>
+                  <li>Dealing with multiple-choice form fields</li>
+                </ul>
+              </div>
+            </section>
+
+            <!-- certificate 3 -->
+            <section class="certificate-item">
+              <div class="certificate-details">
+                <h3>PHP: Object-Oriented Programming</h3>
+                <p>Completed this course on January 19, 2020</p>
+              </div>
+              <div class="certificate-summary">
+                <p>Learned Topic:</p>
+                <ul>
+                  <li>Defining classes</li>
+                  <li>Calling methods</li>
+                  <li>Class inheritance</li>
+                  <li>Extending and overriding classes</li>
+                  <li>Accessing and controlling access to properties and methods</li>
+                  <li>Static properties and methods</li>
+                  <li>Magic methods: constructor, destructor, and clone</li>
+                  <li>Creating a PHP OOP project</li>
+                </ul>
+              </div>
+            </section>
+        </div>
+
+      </section>
+
+      <!-- *****************  CONTACT INFO / SOCIAL MEDIA  ***************** -->
+      <footer>
+        <div class="content-wrap">
+          <h2>Let's Keep in Touch!</h2>
+
+          <!-- Social media and contact links. Add or remove any networks. -->
+          <ul class="contact-list">
+            <li><a href="mailto:rkrakesh133@gmail.com">rkrakesh133@gmail.com</a></li>
+            <!-- <li><a href="http://christinatruong.com" target="_blank">christinatruong.com</a></li> -->
+            <li><a href="https://facebook.com/rakeshbroy" target="_blank">Facebook</a></li>
+            <li><a href="https://www.linkedin.com/in/rakesh-b-roy-856437199/" target="_blank">LinkedIn</a></li>
+          </ul>
+        </div>
+      </footer>
+    </main>
+  </body>
+</html>
